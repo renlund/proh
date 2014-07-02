bf2a089cb57700d2e567e4a34f39eaa1c05a4cd2
Henrik Renlund <henrikrenlund@gmail.com>
2014-07-02 15:20:54 +0200

    (dunno)

--- R/fetchAll.r
+++ R/fetchAll.r
@@ -8,12 +8,12 @@
 fetchAll <- function(calc=TRUE, autoload=TRUE){
    LIST <- c()
    if(calc){ 
-      x <- list.files(path='calc', recursive=FALSE, full.names=TRUE, include.dirs=FALSE)
+      x <- list.files(path='calc', recursive=FALSE, full.names=TRUE, include.dirs=FALSE, pattern="(.rdat)|(.rdata)")
       x <- setdiff(x, file.path('calc', 'autoload'))
       LIST <- c(LIST, x)
    }
    if(autoload) 
-      LIST <- c(LIST, list.files(path=file.path('calc','autoload'), recursive=FALSE, full.names=TRUE))
+      LIST <- c(LIST, list.files(path=file.path('calc','autoload'), pattern="(.rdat)|(.rdata)", recursive=FALSE, full.names=TRUE, all.files=TRUE))
    for(L in LIST) load(L, envir=.GlobalEnv)
    invisible(NULL)
 }
