2039bf8701bfb5870b2b8e5003e27db591fbc8ee
Henrik <henrik.renlund@ucr.uu.se>
2014-11-20 12:33:16 +0100

    removed unnecessary dependence

--- R/shoWes.r
+++ R/shoWes.r
@@ -1,19 +1,22 @@
 #'  @title Show available Wes Andersson palettes
 #'  @description Show available Wes Andersson palettes
-#'  @import wesanderson
 #'  @examples
 #'  shoWes()
 #'  @export
 
 shoWes <- function(){
-  df <- namelist
-  n <- nrow(df)
-  N <- max(df$wesnums)
-  plot(1,1, xlim=c(0,N+2.5), ylim=c(1,n), type='n', xaxt='n', yaxt='n', bty='n', xlab="", ylab="")
-  for(k in 1:n){
-    namn <-  df$movies[k]
-    wn   <-  df$wesnums[k]
-    text(x=1, y=k, namn)
-    points(x=3:(3+wn-1), y=rep(k, wn), pch=21, cex=5, bg=wesanderson::wes.palette(n=wn, name=namn))
-  }
+   if(require("wesanderson")){
+      df <- namelist
+      n <- nrow(df)
+      N <- max(df$wesnums)
+      plot(1,1, xlim=c(0,N+2.5), ylim=c(1,n), type='n', xaxt='n', yaxt='n', bty='n', xlab="", ylab="")
+      for(k in 1:n){
+         namn <-  df$movies[k]
+         wn   <-  df$wesnums[k]
+         text(x=1, y=k, namn)
+         points(x=3:(3+wn-1), y=rep(k, wn), pch=21, cex=5, bg=wesanderson::wes.palette(n=wn, name=namn))
+      }
+   } else {
+      message("[shoWes] package:wesanderson is not installed")
+   }
 }
