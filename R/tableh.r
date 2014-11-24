02d285d6623eb6eea6f88cb37e5862ef57439c48
Henrik <henrik.renlund@ucr.uu.se>
2014-11-24 15:52:58 +0100

    'tableh' has option 'attach_table' also locally

--- R/tableh.r
+++ R/tableh.r
@@ -1,17 +1,19 @@
 #' @title Create table with attached info
 #' @description \code{Hmisc::latex} and object, write to file and attach
 #' @param object usually a data frame or matrix
+#' @param attach_table logical; should the table be attached? if missing this
+#'   will be determined by \code{opts_proh$get("attach_table")}
 #' @param label a label 'tab:<label>' will be set in \code{latex}
-#' @param fun what function to use to write \code{object} to file 'table/<label>.txt'.
+#' @param fun what function to use to write \code{object} to file
+#'   'table/<label>.txt'.
 #' @param caption caption for \code{latex}
 #' @param data if attach data is something other than object
 #' @param ... arguments passed to \code{latex}
 #' @importFrom Hmisc latex
 #' @export
 
-
-tableh <- function(object, label, fun=write.csv, caption, data, ...){
-   if(opts_proh$get("attach_table")[[1]]){
+tableh <- function(object, attach_table=FALSE, label, fun=write.csv, caption, data, ...){
+   if(opts_proh$get("attach_table")[[1]] | attach_table){
       if(missing(data)) data <- object
       if(!dir.exists("table")) dir.create("table")
       file_path <- file.path("table", paste0(label, ".txt"))
@@ -24,6 +26,7 @@ tableh <- function(object, label, fun=write.csv, caption, data, ...){
       label = paste0("tab:", label),
       caption = caption,
       where = "htb",
+      caption.lot = caption,
       ...
    )
 }
