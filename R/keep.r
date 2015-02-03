ddfa1375c891e9b7d3c5859e9b678c6711148f62
Henrik Renlund <henrikrenlund@gmail.com>
2015-02-03 07:25:31 +0100

    keep checks (under windows) for filename fails, e.g. trying to keep 'A' but 'a.rdat' already exists now will generate an error. Also, some simple test is added

--- R/keep.r
+++ R/keep.r
@@ -1,4 +1,4 @@
-#' @title Save 
+#' @title Save
 #' @description Save an object to 'calc' folder
 #' @author Henrik Renlund
 #' @param name character; the name of one or more variable
@@ -6,11 +6,15 @@
 #' @export
 
 keep <- function(name, autoload=FALSE){
-  location <- if(autoload) file.path('calc', 'autoload') else 'calc' 
-  if(!is.character(name)) 
+  location <- if(autoload) file.path('calc', 'autoload') else 'calc'
+  if(!is.character(name))
       stop("[proh::keep] 'name' should be the names (as a character vector) of variables.")
    for(K in name){
       if(exists(K, envir=.GlobalEnv)){
+         L <- list.files(path = "calc")
+         if(any(grepl(K, L, ignore.case=TRUE)) & !any(grepl(K, L))){
+            if(Sys.info()['sysname'] == "Windows") stop("[proh::keep] Windows does not distinguish between upper- and lower case in filenames and there is a similar file kept in 'calc/'.")
+         }
          save(list=K, envir=.GlobalEnv, file=file.path(location, paste0(K, ".rdat")))
       } else {
          warning(paste0("[proh::keep] '", K, "' does not exists."))
---
+++ inst/tests/test_keep.r
@@ -0,0 +1,18 @@
+context("testing 'keep'")
+
+test_that("'keep' workds", {
+   dir <- paste0("TMP_DIR__", paste( sample(c(letters,LETTERS,0:9), size=2^6, replace = TRUE), collapse=""))
+   dir.create(path = dir)
+   old_dir <- setwd(dir)
+   dir.create("calc")
+   dir.create("calc/autoload")
+   test <- 1
+   keep("test")
+   expect_equal(list.files("calc/"), c("autoload", "test.rdat"))
+   a <- "string"
+   A <- "another_string"
+   keep("a")
+   if(Sys.info()['sysname'] == "Windows") expect_error(keep("A"))
+   setwd(old_dir)
+   unlink(x = dir, recursive = TRUE)
+})
