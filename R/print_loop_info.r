bf301150bf04990ad75c5e32459f4b6e9a1ff210
Henrik Renlund <henrikrenlund@gmail.com>
2015-02-27 08:26:54 +0100

    prefix + suffix addition to print_loop_info

--- R/print_loop_info.r
+++ R/print_loop_info.r
@@ -3,10 +3,15 @@
 #' @param i loopin index (\code{i in 1:n})
 #' @param n length of looping index
 #' @param len number of times to get progress information
+#' @example
+#' for(i in 1:1E3){
+#'    print_loop_info(i, n)
+#'    print_loop_info(i, n, len = 20, "       ", " <yeah>")
+#' }; rm(i)
 #' @export
 
-print_loop_info <- function(i,n,len=10){
+print_loop_info <- function(i, n, len = 10, prefix = "", suffix = ""){
    if(i %in% floor(seq(1, n, length.out=len+1)[-1])){
-      cat(round(100*i/n), "percent done\n")
+      cat(prefix, round(100*i/n), "percent done.", suffix, "\n")
    }
 }
