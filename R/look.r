16ea1dddd9a6038b433ef0af40d4037e28b68f2a
unknown <henre358@UCR-5CG5162TP6.user.uu.se>
2015-08-19 13:21:03 +0200

    proh::look <> remove unimplemented argument 'sumatra' and replace with new argument 'file'

--- R/look.r
+++ R/look.r
@@ -1,28 +1,11 @@
 #' @title View the pdf
-#' @description Open the current pdf version of the rapport
+#' @description Open the current pdf version of the rapport (or indeed try to
+#' open any file, as this is just a wrapper for \code{shell.exec})
 #' @author Henrik Renlund
-#' @param sumatra not implemented yet, will be FALSE
+#' @param file filename (default 'rapport.pdf')
 #' @export
 
-look <- function(sumatra=FALSE){
-   if(!identical(sumatra, FALSE)){
-      warning("[proh::Pdf] sumatra!=FALSE currently unsupported")
-      sumatra <- FALSE
-   }
-   if(sumatra){
-      sumatra_path <- opts_proh$get("sumatra_path")[[1]]
-      sumatra <- file.path(sumatra_path,"SumatraPDF.exe")
-      if(!file.exists(sumatra)){
-         warning("[proh::View] SumatraPDF.exe does not exists in default windows location. You can change the path with 'opts_proh$set'.")
-      } else {
-         wd <- getwd()
-         rapport <- file.path(wd, "rapport.pdf", fsep="\\")
-         setwd(sumatra_path)
-         shell.exec(paste("SumatraPDF.exe", rapport))
-         setwd(wd)
-         return(invisible(NULL))
-      }
-   }
-   shell.exec("rapport.pdf")
+look <- function(file = "rapport.pdf"){
+   shell.exec(file)
    invisible(NULL)
 }
